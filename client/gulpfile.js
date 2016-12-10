a80ab731f185999eb569747ae14315f31564ff11
Gabriel <gabriel@multiverso.me>
2016-12-10 10:41:55 -0200

    Add build and watch gulp tasks

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,8 +1,25 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp    = require('gulp');
+var gulpIf  = require('gulp-if');
+var sass    = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var useref  = require('gulp-useref');
+var uglify  = require('gulp-uglify');
 
 gulp.task('sass', function(){
-  return gulp.src('app/scss/styles.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
+  return gulp.src('src/scss/styles.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
 });
+
+gulp.task('build', function(){
+  gulp.start('sass');
+  return gulp.src('src/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+})
