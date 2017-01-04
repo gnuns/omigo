d806bbd3b477e41a4925d41ede99f3b697745ac4
Gabriel <gabriel@multiverso.me>
2017-01-04 16:41:29 -0200

    :satisfied: HABEMUS CHATUM SERVUR

--- chat/client.js
+++ chat/client.js
@@ -2,28 +2,68 @@
 module.exports = client;
 
 function client(socket) {
-  let partnerInfoMap = {};
+  let id = socket.id;
+  let partnersMap = {};
+  let currentPartner = false;
+  let isWaiting = true;
 
   return {
-    id: socket.id,
+    id: id,
     socket: socket,
     setPartnerInfo: setPartnerInfo,
-    isValidPartner: isValidPartner
+    isValidPartner: isValidPartner,
+    isWaiting: () => isWaiting,
+    waitNext: waitNext,
+    sendSystemInfo: sendSystemInfo,
+    sendMessageToPartner: sendMessageToPartner,
+    disconnectFromPartner: disconnectFromPartner
   };
 
   function setPartnerInfo(partner) {
     let currentTimestamp = Math.floor(Date.now() / 1000);
-    if (!partnerInfoMap[partner.id]) partnerInfoMap[partner.id] = {chatsCount:0};
-    partnerInfoMap[partner.id].lastChatTimestamp = currentTimestamp;
-    partnerInfoMap[partner.id].chatsCount++;
+    if (!partnersMap[partner.id]) partnersMap[partner.id] = {chatsCount:0};
+    partnersMap[partner.id].lastChatTimestamp = currentTimestamp;
+    partnersMap[partner.id].chatsCount++;
+    isWaiting = false;
+    partner.sendSystemInfo('partner_connected');
+    // console.log(socket.id, 'is the new partner of', partner.id);
+    currentPartner = partner;
   }
 
-  function isValidPartner(partnerId) {
-    let currentTimestamp = Math.floor(Date.now() / 1000);
-    let isReturningPartner = !!partnerInfoMap[partnerId];
-    if ((partnerId === socket.id)) return false;
-    if (!isReturningPartner) return true;
-    if ((currentTimestamp - partnerInfoMap[partnerId].lastChatTimestamp) > 300) return true;
-    return false;
+  function disconnectFromPartner() {
+    if (currentPartner) {
+      currentPartner.sendSystemInfo('partner_disconnected');
+    }
+    currentPartner = false;
+  }
+
+  function sendMessageToPartner(message) {
+    let msg = {
+      'from': 'partner',
+      'content': message
+    };
+    if (currentPartner) {
+      socket.broadcast.to(currentPartner.id).emit('msg', msg);
+    }
+  }
+
+  function sendSystemInfo(code) {
+    socket.emit('sysinfo', code);
+  }
+
+  function waitNext() {
+    isWaiting = true;
+  }
+
+  function isValidPartner(partner) {
+    if (partner.id !== socket.id && partner.isWaiting()) {
+      let isReturningPartner = !!partnersMap[partner.id];
+      if (isReturningPartner) {
+        let now = Math.floor(Date.now() / 1000);
+        let timeSinceLastChat = (now - partnersMap[partner.id].lastChatTimestamp);
+        // if timeSinceLastChat > 5 minutes
+        return (timeSinceLastChat > 300);
+      } else return true;
+    } else return false;
   }
 }
--- chat/handler.js
+++ chat/handler.js
@@ -4,31 +4,57 @@ module.exports.init = init;
 const _ = require('lodash');
 let Client = require('./client');
 let clientList = [];
+let waitingList = [];
 let io;
 
 function init(_io) {
   io = _io;
   io.on('connection', onConnet);
+  setInterval(refreshConsole, 700)
+}
+
+function refreshConsole() {
+  process.stdout.write('Clients: ' + clientList.length +
+                       '| Waiting: ' + waitingList.length + '\r');
 }
 
 function onConnet(socket) {
   let client = new Client(socket);
-  console.log(client.id, ' connected');
+  // console.log(client.id, ' connected');
   socket.on('disconnect', () => onDisconnect(client));
   socket.on('next', () => onRequestNext(client));
+  socket.on('msg', (msg) => onMessage(client, msg));
   clientList.push(client);
+  // console.log(waitingList.length);
 }
 
 function onDisconnect(client) {
-  console.log(client.id, ' disconnected');
-  clientList = _.remove(clientList, (c) => c.id === client.id);
+  // console.log(client.id, ' disconnected');
+  client.disconnectFromPartner();
+  _.remove(clientList, (c) => c.id === client.id);
+  _.remove(waitingList, (c) => c.id === client.id);
 }
 
-function onRequestNext(requesterClient) {
-  let partner = clientList[_.random(0, clientList.length)];
-  while (!requesterClient.isValidPartner(partner.id)) {
-    partner = clientList[_.random(0, clientList.length)];
+function onMessage(client, msg) {
+  client.sendMessageToPartner(msg);
+}
+
+function onRequestNext(client) {
+  let partner = false;
+  for (let possiblePartner of waitingList) {
+    if (client.isValidPartner(possiblePartner)) {
+      partner = possiblePartner;
+      break;
+    }
   }
-  partner.setPartnerInfo(requesterClient);
-  requesterClient.setPartnerInfo(partner);
+  if (!partner) {
+    if (!_.some(waitingList, ['id', client.id])) {
+      client.waitNext();
+      waitingList.push(client);
+    }
+  } else {
+    _.remove(waitingList, (c) => c.id === client.id || c.id === partner.id);
+    client.setPartnerInfo(partner);
+    partner.setPartnerInfo(client);
+  }
 }
