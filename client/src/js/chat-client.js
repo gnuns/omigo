51dd0a8f45a9f0e3985a7bf2df3375e42b4e2582
Gabriel Nunes <ghsn@ufmg.br>
2017-07-06 20:24:37 -0300

    Remove STUN server
    Firefox...

--- client/src/js/chat-client.js
+++ client/src/js/chat-client.js
@@ -128,8 +128,7 @@ window.chatClient = (function() {
       'iceServers': [{'url': 'stun:stun.l.google.com:19302'},
                      {'url': 'stun:stun1.l.google.com:19302'},
                      {'url': 'stun:stun2.l.google.com:19302'},
-                     {'url': 'stun:stun3.l.google.com:19302'},
-                     {'url': 'stun:stun4.l.google.com:19302'}]});
+                     {'url': 'stun:stun3.l.google.com:19302'}]});
 
     peer.addStream(localMediaStream);
     peer.on('addStream', function (event) {
